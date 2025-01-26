b3adefb4b293ba7cd5c736cdafeed3ae9318ed27
soledad <104692706+Soledadrs@users.noreply.github.com>
2025-01-26 23:51:27 +0800

    Add files via upload

---
+++ SoledadHomePage/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import "@/style/style.scss";
+import App from "@/App.vue";
+// 引入 pinia
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+// swiper
+import "swiper/css";
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
+app.mount("#app");
+
+// PWA
+navigator.serviceWorker.addEventListener("controllerchange", () => {
+  // 弹出更新提醒
+  console.log("站点有啲小更新喔，唔该刷新页面就搞掂");
+  ElMessage("站点有啲小更新喔，唔该刷新页面就搞掂");
+});
